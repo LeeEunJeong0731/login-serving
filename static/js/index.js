27b20583dd1b1b6a4c85c58ea69559a9e6da9c96
leeeunjeong <eunjeong990731@gmail.com>
2023-10-18 17:57:04 +0900

    유효성 검사 실패...

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,21 @@
-console.log("hi");
+const root = document.getElementById("root");
+const submit = document.getElementById("submit");
+const form = document.getElementById("form");
+
+const userId = root.children[0].children[0];
+const password = root.children[0].children[1];
+const password2 = root.children[0].children[2];
+const email = root.children[0].children[3];
+
+const idRegExp = /^[a-zA-z]$/;
+//로그인 유효성 검사 소,대문자 또한 0-9까지 사용 할 수있다.
+submit.addEventListener("click", () => {
+  if (userId.value === "") {
+    alert("ID를 입력해주세요.");
+    return false;
+  }
+  if (!idRegExp.test(userId.value)) {
+    alert("ID는 대소문자 포함으로 입력해주세요.");
+    return false;
+  }
+});
