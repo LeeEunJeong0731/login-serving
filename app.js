feb0776c5490ef3d2afb7b109d696a97d8e4f8a4
leeeunjeong <eunjeong990731@gmail.com>
2023-10-18 15:39:44 +0900

    스타일 시트 연결 완료

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { "Content-type": "text/html" });
       res.end(data);
     });
+  } else if (req.url === "/css/style.css" && req.method === "GET") {
+    fs.readFile("./static/css/style.css", "utf8", (err, data) => {
+      if (err) {
+        serverErrorLog();
+      }
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(data);
+    });
   }
 });
 const PORT = 8080;
