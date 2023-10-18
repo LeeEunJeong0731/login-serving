8c9f6085d5d4f6beb602eacd602b6b7cbe6da5a5
leeeunjeong <eunjeong990731@gmail.com>
2023-10-18 19:03:40 +0900

    자바스크립트 연결 된 줄 알았으나 연결 되지 않음

--- static/js/index.js
+++ static/js/index.js
@@ -9,13 +9,4 @@ const email = root.children[0].children[3];
 
 const idRegExp = /^[a-zA-z]$/;
 //로그인 유효성 검사 소,대문자 또한 0-9까지 사용 할 수있다.
-submit.addEventListener("click", () => {
-  if (userId.value === "") {
-    alert("ID를 입력해주세요.");
-    return false;
-  }
-  if (!idRegExp.test(userId.value)) {
-    alert("ID는 대소문자 포함으로 입력해주세요.");
-    return false;
-  }
-});
+console.log("hi");
