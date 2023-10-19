10c0367394d47a534f2ef32eb6218e24a1b108c9
leeeunjeong <eunjeong990731@gmail.com>
2023-10-19 18:10:01 +0900

    subpage만

--- static/js/indexF.js
+++ static/js/indexF.js
@@ -1,42 +1,4 @@
-const OneHTMl = `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/style.css" />
-  </head>
-  <body>
-    <div id="root">
-      <form name="joinForm"  method="post">
-        <div>
-          <label>ID <input type="text" name="username" id="userId" /></label>
-        </div>
-        <div>
-          <label
-            >PW <input type="password" name="password" id="password"
-          /></label>
-        </div>
-        <div>
-          <label
-            >PW <input type="password" name="password" id="repassword"
-          /></label>
-        </div>
-        <div>
-          <label>Email <input type="email" name="email" id="email" /></label>
-        </div>
-        <input
-          type="submit"
-          value="Sing Up"
-          id="submit"
-          onclick="submit_check()"
-        />
-      </form>
-    </div>
-    </body>
-    </html>`;
-
-const TwoHTML = `<!DOCTYPE html>
+const sub = `<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
@@ -68,3 +30,5 @@ const TwoHTML = `<!DOCTYPE html>
       </body>
     </html>
     `;
+
+module.exports = indexF;
