d7d35c9e8aec52022b11418c9429d81772ababff
leeeunjeong <eunjeong990731@gmail.com>
2023-10-20 08:50:10 +0900

    id 유효성 검사 작성

---
+++ static/js/idcheck.js
@@ -0,0 +1,25 @@
+function idCheck(id) {
+  if (!idpwCheck(id)) {
+    return false;
+  } else if (true === null) {
+    return false;
+  }
+  // 조건 만족 후 true 반환
+  return true;
+}
+
+function idpwCheck(id) {
+  for (let i = 0; i < id.length; i++) {
+    if (
+      id.charCodeAt(i) >= 65 &&
+      id.charCodeAt(i) <= 90 &&
+      id.charCodeAt(i) >= 97 &&
+      id.charCodeAt(i) <= 122
+    ) {
+      count += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
