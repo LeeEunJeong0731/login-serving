bb27ab622d24e2347a41299bec51df43afd6f29c
leeeunjeong <eunjeong990731@gmail.com>
2023-10-19 15:43:11 +0900

    id pw유효성 검사 모듈화

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
-function idAlpha(str) {
-  var code;
-  for (var i = 0; i < str.length; i++) {
-    code = str.charCodeAt(i);
+function idAlpha(value) {
+  let code;
+  for (let i = 0; i < value.length; i++) {
+    code = value.charCodeAt(i);
     if (
       !(code > 64 && code < 91) && // upper alpha( A - Z )
       !(code > 96 && code < 123)
@@ -13,4 +13,20 @@ function idAlpha(str) {
   return true;
 }
 
-// (code > 47 && code < 58) && // numeric (0-9)
+function pwAlphaNum(value) {
+  code;
+  for (let i = 0; i < value, length; i++) {
+    code = value.charCodeAt(i);
+    if (
+      !(code > 64 && code < 91) &&
+      !(code > 96 && code < 123) &&
+      !(code > 47 && code < 58) // numeric (0-9)
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+module.exports = idAlpha;
+module.exports = pwAlphaNum;
