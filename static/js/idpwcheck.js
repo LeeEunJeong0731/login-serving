6245ebf47d9a4001f2f07218fcc5108b5f8554c5
leeeunjeong <eunjeong990731@gmail.com>
2023-10-23 09:40:07 +0900

    오타수정

--- static/js/idpwcheck.js
+++ static/js/idpwcheck.js
@@ -1,6 +1,6 @@
 function idpwcheck(id) {
   upper = false; // 대문자
-  rower = false; // 소문자
+  lower = false; // 소문자
 
   for (let i = 0; i < id.length; i++) {
     code = id.charCodeAt(i);
@@ -8,7 +8,7 @@ function idpwcheck(id) {
     if (code >= 65 && code <= 90) {
       upper = true; //대문자 true 반환
     } else if (code >= 97 && code <= 122) {
-      rower = true; //소문자 true 반환
+      lower = true; //소문자 true 반환
     }
   }
   return upper && lower; // 대소문자가 모두 포함되면 true, 아니면 false
